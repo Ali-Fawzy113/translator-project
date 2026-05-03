2d26f4f4141034a21bc695941330b1c23f888bed
Ali-Fawzy113 <202404104@pua.edu.eg>
2026-05-03 17:28:53 +0300

    Add files via upload

--- App.py
+++ App.py
@@ -1,18 +1,19 @@
-# import streamlit as st
-# st.title("Hello streamlit")
-# st.write("! ده اول تطبيق ليك")
-print("*******************************")
-# import streamlit as st
-# st.title("Translator Project 🌍")
-# text = st.text_input("Enter your sentences:")
-# if text:
-#     st.write(text)
-print("*******************************")
 import streamlit as st
 from deep_translator import GoogleTranslator
+
 st.title("Translator Project 🌍")
-text = st.text_input("Enter your sentences:")
+st.markdown("Project by Ali Aboielkher")
+if st.button("About"):
+    st.info("مرحبا بكم في مشروع يترجم من اللغة العربية للغة الانجليزية والعكس")
+option = st.selectbox(
+    "Select Target Language (اختر لغة الترجمة):",
+    ("Arabic (العربية)", "English (الإنجليزية)"))
+
+target_lang = 'ar' if option == "Arabic (العربية)" else 'en'
+
+text = st.text_input("Enter your sentences / أدخل جملتك:")
+
 if text:
-    translation = GoogleTranslator(source='auto', target='ar').translate(text)
+    translation = GoogleTranslator(source='auto', target=target_lang).translate(text)
     st.subheader("Translation:")
     st.success(translation)
